bef26eeb77efdb2a7a08fd4da32f5dc95802f072
jisu2433 <93359953+jisu2433@users.noreply.github.com>
2022-06-14 23:11:22 +0900

    Add : 스크롤 높이 세팅

---
+++ main.js
@@ -0,0 +1,45 @@
+(() => {
+  const sceneInfo = [
+    {
+      type: 'sticky',
+      heightNum: 5,
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector('#scroll-section-0')
+      }
+    },
+    {
+      type: 'normal',
+      heightNum: 5,
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector('#scroll-section-1')
+      }
+    },
+    {
+      type: 'sticky',
+      heightNum: 5,
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector('#scroll-section-2')
+      }
+    },
+    {
+      type: 'sticky',
+      heightNum: 5,
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector('#scroll-section-3')
+      }
+    }
+  ];
+
+  function setLayout() {
+    for (let i = 0; i < sceneInfo.length; i++) {
+      sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * innerHeight;
+      sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+    }
+    console.log(sceneInfo);
+  }
+  window.addEventListener('resize', setLayout);
+})();
