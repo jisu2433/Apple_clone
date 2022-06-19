44ccba717bf79f3bd583bbafcd9ec1aba6e70746
jisu2433 <93359953+jisu2433@users.noreply.github.com>
2022-06-19 22:30:15 +0900

    Add : scroll-section-3 인터랙션 구현

--- main.js
+++ main.js
@@ -97,10 +97,19 @@
       scrollHeight: 0,
       objs: {
         container: document.querySelector('#scroll-section-3'),
-        canvasCaption: document.querySelector('.canvas-caption')
+				canvasCaption: document.querySelector('.canvas-caption'),
+				canvas: document.querySelector('.image-blend-canvas'),
+				context: document.querySelector('.image-blend-canvas').getContext('2d'),
+				imagesPath: [
+					'./images/blend-image-1.jpg',
+					'./images/blend-image-2.jpg'
+				],
+				images: []
       },
       values: {
-
+				rect1X: [0, 0, { start: 0, end: 0 }],
+				rect2X: [0, 0, { start: 0, end: 0 }],
+				rectStartY:0
       }
     }
   ];
@@ -117,7 +126,15 @@
       imgElem2 = new Image();
       imgElem2.src = `./video/002/IMG_${7027 + i}.JPG`;
       sceneInfo[2].objs.videoImages.push(imgElem2);
-    }
+		}
+		
+		let imgElem3;
+		for (let i = 0; i < sceneInfo[3].objs.imagesPath.length; i++){
+			imgElem3 = new Image();
+			imgElem3.src = sceneInfo[3].objs.imagesPath[i];
+      sceneInfo[3].objs.images.push(imgElem3);
+		}
+		console.log(sceneInfo[3].objs.images);
   }
   setCanvasImages();
 
@@ -323,7 +340,7 @@
 				break;
 
 			case 3:
-				// console.log('3 play');
+				console.log('3 play');
 				let step = 0;
 				// 가로/세로 모두 꽉 차게 하기 위해 여기서 세팅(계산 필요)
 				const widthRatio = window.innerWidth / objs.canvas.width;
@@ -346,8 +363,8 @@
 				const recalculatedInnerWidth = document.body.offsetWidth / canvasScaleRatio;
 				const recalculatedInnerHeight = window.innerHeight / canvasScaleRatio;
 
+				console.log; (objs.canvas.getBoundingClientRect());
 				if (!values.rectStartY) {
-					// values.rectStartY = objs.canvas.getBoundingClientRect().top;
 					values.rectStartY = objs.canvas.offsetTop + (objs.canvas.height - objs.canvas.height * canvasScaleRatio) / 2;
 					values.rect1X[2].start = (window.innerHeight / 2) / scrollHeight;
 					values.rect2X[2].start = (window.innerHeight / 2) / scrollHeight;
@@ -375,54 +392,54 @@
 					objs.canvas.height
 				);
 
-				if (scrollRatio < values.rect1X[2].end) {
-					step = 1;
-					// console.log('캔버스 닿기 전');
-					objs.canvas.classList.remove('sticky');
-				} else {
-					step = 2;
-					// console.log('캔버스 닿은 후');
-					// 이미지 블렌드
-					// values.blendHeight: [ 0, 0, { start: 0, end: 0 } ]
-					values.blendHeight[0] = 0;
-					values.blendHeight[1] = objs.canvas.height;
-					values.blendHeight[2].start = values.rect1X[2].end;
-					values.blendHeight[2].end = values.blendHeight[2].start + 0.2;
-					const blendHeight = calcValues(values.blendHeight, currentYOffset);
+				// if (scrollRatio < values.rect1X[2].end) {
+				// 	step = 1;
+				// 	// console.log('캔버스 닿기 전');
+				// 	objs.canvas.classList.remove('sticky');
+				// } else {
+				// 	step = 2;
+				// 	// console.log('캔버스 닿은 후');
+				// 	// 이미지 블렌드
+				// 	// values.blendHeight: [ 0, 0, { start: 0, end: 0 } ]
+				// 	values.blendHeight[0] = 0;
+				// 	values.blendHeight[1] = objs.canvas.height;
+				// 	values.blendHeight[2].start = values.rect1X[2].end;
+				// 	values.blendHeight[2].end = values.blendHeight[2].start + 0.2;
+				// 	const blendHeight = calcValues(values.blendHeight, currentYOffset);
 
-					objs.context.drawImage(objs.images[1],
-						0, objs.canvas.height - blendHeight, objs.canvas.width, blendHeight,
-						0, objs.canvas.height - blendHeight, objs.canvas.width, blendHeight
-					);
+				// 	objs.context.drawImage(objs.images[1],
+				// 		0, objs.canvas.height - blendHeight, objs.canvas.width, blendHeight,
+				// 		0, objs.canvas.height - blendHeight, objs.canvas.width, blendHeight
+				// 	);
 
-					objs.canvas.classList.add('sticky');
-					objs.canvas.style.top = `${-(objs.canvas.height - objs.canvas.height * canvasScaleRatio) / 2}px`;
+				// 	objs.canvas.classList.add('sticky');
+				// 	objs.canvas.style.top = `${-(objs.canvas.height - objs.canvas.height * canvasScaleRatio) / 2}px`;
 
-					if (scrollRatio > values.blendHeight[2].end) {
-						values.canvas_scale[0] = canvasScaleRatio;
-						values.canvas_scale[1] = document.body.offsetWidth / (1.5 * objs.canvas.width);
-						values.canvas_scale[2].start = values.blendHeight[2].end;
-						values.canvas_scale[2].end = values.canvas_scale[2].start + 0.2;
+				// 	if (scrollRatio > values.blendHeight[2].end) {
+				// 		values.canvas_scale[0] = canvasScaleRatio;
+				// 		values.canvas_scale[1] = document.body.offsetWidth / (1.5 * objs.canvas.width);
+				// 		values.canvas_scale[2].start = values.blendHeight[2].end;
+				// 		values.canvas_scale[2].end = values.canvas_scale[2].start + 0.2;
 
-						objs.canvas.style.transform = `scale(${calcValues(values.canvas_scale, currentYOffset)})`;
-						objs.canvas.style.marginTop = 0;
-					}
+				// 		objs.canvas.style.transform = `scale(${calcValues(values.canvas_scale, currentYOffset)})`;
+				// 		objs.canvas.style.marginTop = 0;
+				// 	}
 
-					if (scrollRatio > values.canvas_scale[2].end
-						&& values.canvas_scale[2].end > 0) {
-						objs.canvas.classList.remove('sticky');
-						objs.canvas.style.marginTop = `${scrollHeight * 0.4}px`;
+				// 	if (scrollRatio > values.canvas_scale[2].end
+				// 		&& values.canvas_scale[2].end > 0) {
+				// 		objs.canvas.classList.remove('sticky');
+				// 		objs.canvas.style.marginTop = `${scrollHeight * 0.4}px`;
 
-						values.canvasCaption_opacity[2].start = values.canvas_scale[2].end;
-						values.canvasCaption_opacity[2].end = values.canvasCaption_opacity[2].start + 0.1;
-						values.canvasCaption_translateY[2].start = values.canvasCaption_opacity[2].start;
-						values.canvasCaption_translateY[2].end = values.canvasCaption_opacity[2].end;
-						objs.canvasCaption.style.opacity = calcValues(values.canvasCaption_opacity, currentYOffset);
-						objs.canvasCaption.style.transform = `translate3d(0, ${calcValues(values.canvasCaption_translateY, currentYOffset)}%, 0)`;
-					} else {
-						objs.canvasCaption.style.opacity = values.canvasCaption_opacity[0];
-					}
-				}
+				// 		values.canvasCaption_opacity[2].start = values.canvas_scale[2].end;
+				// 		values.canvasCaption_opacity[2].end = values.canvasCaption_opacity[2].start + 0.1;
+				// 		values.canvasCaption_translateY[2].start = values.canvasCaption_opacity[2].start;
+				// 		values.canvasCaption_translateY[2].end = values.canvasCaption_opacity[2].end;
+				// 		objs.canvasCaption.style.opacity = calcValues(values.canvasCaption_opacity, currentYOffset);
+				// 		objs.canvasCaption.style.transform = `translate3d(0, ${calcValues(values.canvasCaption_translateY, currentYOffset)}%, 0)`;
+				// 	} else {
+				// 		objs.canvasCaption.style.opacity = values.canvasCaption_opacity[0];
+				// 	}
+				// }
 
 				break;
       }
